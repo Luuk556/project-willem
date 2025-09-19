9dcfa4c4a3ca4377546c6eec826bd8635316e58a
1080444-Airto <1080444@hr.nl>
2025-09-19 13:04:57 +0200

    Dashboard done
    Install Fontawesome
    Scss dashboard done
    html dashboard done

--- calender/src/screens/admin/dashboard.jsx
+++ calender/src/screens/admin/dashboard.jsx
@@ -1,79 +1,172 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
+
 function AdminDashboard() {
     return (
         <div>
-            <header class="header">
-                <section class="home">
-                    <div class="home__list">
+            <header className="header">
+                <section className="home">
+                    <div className="home__list">
                         <p className="home__list--item">Home</p>
                     </div>
                 </section>
-                <nav class="navigation">
-                    <ul class="navigation__list">
-                        <li class="navigation__list--item">Calendar</li>
-                        <li class="navigation__list--item">Rooms</li>
-                        <li class="navigation__list--item">Logout</li>
+                <nav className="navigation">
+                    <ul className="navigation__list">
+                        <li className="navigation__list--item">Calendar</li>
+                        <li className="navigation__list--item">Rooms</li>
+                        <li className="navigation__list--item">Logout</li>
                     </ul>
                 </nav>
             </header>
 
-            <div class="admin">
-                <div class="userlist">
-                    <section class="dashboard-card">
-                        <div class="card-h">
-                            <div class="card-h__title">
+            <div className="admin">
+
+                <div className="userlist">
+                    <section className="dashboard-card">
+                        <div className="card-h">
+                            <div className="card-h__title">
                                 <p className="card-h__title--text">Users</p>
                             </div>
                         </div>
-                        <div class="card-b">
-                            <div class="card-b__row">
-                                <th class="card-b__row--title">Name</th>
-                                <th class="card-b__row--title">Username</th>
-                                <th class="card-b__row--title">E-mail</th>
+
+                        <div className="card-b">
+                            <div className="card-b__header">
+                                <p className="card-b__header--title">Name</p>
+                                <p className="card-b__header--title">Username</p>
+                                <p className="card-b__header--title">Mail</p>
+                                <p className="card-b__header--title">Edit</p>
                             </div>
-                            <tr>
-                                <td class="card-b__row--text">Airto</td>
-                                <td class="card-b__row--text">Nene</td>
-                                <td class="card-b__row--text">1080444@hr.nl</td>
-                            </tr>
+                            <div className="card-b__body">
+                                <div className="card-b__body--row">
+                                    <p className="card-b__body--text">wim</p>
+                                    <p className="card-b__body--text">willem</p>
+                                    <p className="card-b__body--text">wim@ziggo.nl</p>
+                                    <p className="card-b__body--text"><FontAwesomeIcon icon={faPenToSquare} /></p>
+                                </div>
+                                <div className="card-b__body--row">
+                                    <p className="card-b__body--text">Peter</p>
+                                    <p className="card-b__body--text">Pan</p>
+                                    <p className="card-b__body--text">peter@ziggo.nl</p>
+                                    <p className="card-b__body--text"><FontAwesomeIcon icon={faPenToSquare} /></p>
+                                </div>
+                            </div>
                         </div>
                     </section>
 
-                    <section class="dashboard-card">
-                        <div class="card-h">
-                            <div class="card-h__title">
+                    <section className="dashboard-card">
+                        <div className="card-h">
+                            <div className="card-h__title">
                                 <p className="card-h__title--text">Rooms</p>
                             </div>
                         </div>
-                        <table class="card-b">
-                            <tr class="card-b__row">
-                                <th class="card-b__row--title">Name</th>
-                                <th class="card-b__row--title">Capacity</th>
-                            </tr>
-                            <tr>
-                                <td class="card-b__row--text">Room 101</td>
-                                <td class="card-b__row--text">20</td>
-                                <td class="card-b__row--text">Edit</td>
-                            </tr>
-                        </table>
+
+                        <div className="card-b">
+                            <div className="card-b__header">
+                                <p className="card-b__header--title">Name</p>
+                                <p className="card-b__header--title">Capacity</p>
+                                <p className="card-b__header--title">Edit</p>
+                            </div>
+                            <div className="card-b__body">
+                                <div className="card-b__body--row">
+                                    <p className="card-b__body--text">Room 101</p>
+                                    <p className="card-b__body--text">20</p>
+                                    <p className="card-b__body--text"><FontAwesomeIcon icon={faPenToSquare} /></p>
+                                </div>
+                                <div className="card-b__body--row">
+                                    <p className="card-b__body--text">Room 102</p>
+                                    <p className="card-b__body--text">40</p>
+                                    <p className="card-b__body--text"><FontAwesomeIcon icon={faPenToSquare} /></p>
+                                </div>
+                            </div>
+                        </div>
                     </section>
 
-                    <section class="dashboard-card">
-                        <div class="card-h">
-                            <div class="card-h__title">
+                    <section className="dashboard-card">
+                        <div className="card-h">
+                            <div className="card-h__title">
                                 <p className="card-h__title--text">Events</p>
                             </div>
                         </div>
-                        <table class="card-b">
-                            <tr class="card-b__row">
-                                <th class="card-b__row--title">Name</th>
-                                <th class="card-b__row--title">Date</th>
-                            </tr>
-                            <tr>
-                                <td class="card-b__row--text">Meeting</td>
-                                <td class="card-b__row--text">16-09-2025</td>
-                                <td class="card-b__row--text">Edit</td>
-                            </tr>
-                        </table>
+
+                        <div className="card-b">
+                            <div className="card-b__header">
+                                <p className="card-b__header--title">Name</p>
+                                <p className="card-b__header--title">Date</p>
+                                <p className="card-b__header--title">Edit</p>
+                            </div>
+                            <div className="card-b__body">
+                                <div className="card-b__body--row">
+                                    <p className="card-b__body--text">Meeting</p>
+                                    <p className="card-b__body--text">18-09-2025</p>
+                                    <p className="card-b__body--text"><FontAwesomeIcon icon={faPenToSquare} /></p>
+                                </div>
+                                <div className="card-b__body--row">
+                                    <p className="card-b__body--text">Sprint meeting</p>
+                                    <p className="card-b__body--text">19-09-2025</p>
+                                    <p className="card-b__body--text"><FontAwesomeIcon icon={faPenToSquare} /></p>
+                                </div>
+                                                                <div className="card-b__body--row">
+                                    <p className="card-b__body--text">Meeting</p>
+                                    <p className="card-b__body--text">18-09-2025</p>
+                                    <p className="card-b__body--text"><FontAwesomeIcon icon={faPenToSquare} /></p>
+                                </div>
+                                <div className="card-b__body--row">
+                                    <p className="card-b__body--text">Sprint meeting</p>
+                                    <p className="card-b__body--text">19-09-2025</p>
+                                    <p className="card-b__body--text"><FontAwesomeIcon icon={faPenToSquare} /></p>
+                                </div>
+                                                                <div className="card-b__body--row">
+                                    <p className="card-b__body--text">Meeting</p>
+                                    <p className="card-b__body--text">18-09-2025</p>
+                                    <p className="card-b__body--text"><FontAwesomeIcon icon={faPenToSquare} /></p>
+                                </div>
+                                <div className="card-b__body--row">
+                                    <p className="card-b__body--text">Sprint meeting</p>
+                                    <p className="card-b__body--text">19-09-2025</p>
+                                    <p className="card-b__body--text"><FontAwesomeIcon icon={faPenToSquare} /></p>
+                                </div>
+                                                                <div className="card-b__body--row">
+                                    <p className="card-b__body--text">Meeting</p>
+                                    <p className="card-b__body--text">18-09-2025</p>
+                                    <p className="card-b__body--text"><FontAwesomeIcon icon={faPenToSquare} /></p>
+                                </div>
+                                <div className="card-b__body--row">
+                                    <p className="card-b__body--text">Sprint meeting</p>
+                                    <p className="card-b__body--text">19-09-2025</p>
+                                    <p className="card-b__body--text"><FontAwesomeIcon icon={faPenToSquare} /></p>
+                                </div>
+                                                                <div className="card-b__body--row">
+                                    <p className="card-b__body--text">Meeting</p>
+                                    <p className="card-b__body--text">18-09-2025</p>
+                                    <p className="card-b__body--text"><FontAwesomeIcon icon={faPenToSquare} /></p>
+                                </div>
+                                <div className="card-b__body--row">
+                                    <p className="card-b__body--text">Sprint meeting</p>
+                                    <p className="card-b__body--text">19-09-2025</p>
+                                    <p className="card-b__body--text"><FontAwesomeIcon icon={faPenToSquare} /></p>
+                                </div>
+                                                                <div className="card-b__body--row">
+                                    <p className="card-b__body--text">Meeting</p>
+                                    <p className="card-b__body--text">18-09-2025</p>
+                                    <p className="card-b__body--text"><FontAwesomeIcon icon={faPenToSquare} /></p>
+                                </div>
+                                <div className="card-b__body--row">
+                                    <p className="card-b__body--text">Sprint meeting</p>
+                                    <p className="card-b__body--text">19-09-2025</p>
+                                    <p className="card-b__body--text"><FontAwesomeIcon icon={faPenToSquare} /></p>
+                                </div>
+                                                                <div className="card-b__body--row">
+                                    <p className="card-b__body--text">Meeting</p>
+                                    <p className="card-b__body--text">18-09-2025</p>
+                                    <p className="card-b__body--text"><FontAwesomeIcon icon={faPenToSquare} /></p>
+                                </div>
+                                <div className="card-b__body--row">
+                                    <p className="card-b__body--text">Sprint meeting</p>
+                                    <p className="card-b__body--text">19-09-2025</p>
+                                    <p className="card-b__body--text"><FontAwesomeIcon icon={faPenToSquare} /></p>
+                                </div>
+                            </div>
+                        </div>
                     </section>
                 </div>
             </div>
