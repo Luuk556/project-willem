058f6db094fe3ed5801293a8cb27cbe06e5b0d59
luuk556 <luukweeda.2001@gmail.com>
2025-09-19 15:19:30 +0200

    Merged with dev.

---
+++ calender/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import './styling/style.css';
+import Login from './screens/Login/Login';
+import AdminDashboard from './screens/admin/dashboard';
+
+function App() {
+  return (
+    <div className="App">
+
+        <AdminDashboard></AdminDashboard>
+      
+    </div>
+  );
+}
+
+export default App;
