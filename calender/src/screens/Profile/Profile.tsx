285dcdadfd7576d3f49390b78b3aa0501cf254b0
Kai <kaitholen@outlook.com>
2025-10-07 13:33:26 +0200

    edit button werkt nog niet

--- calender/src/screens/Profile/Profile.tsx
+++ calender/src/screens/Profile/Profile.tsx
@@ -1,20 +1,41 @@
 import React from "react"
 import "./Profile.css"
 
+let editing: boolean = false;
+
 interface ProfileProps{fieldname: string}
 
 const Field: React.FC<ProfileProps> = ({fieldname = "n/a"}) => {
+    let input_type: string = "text"
+    let placeholder: string; // haal value op uit database?
+
+    if (fieldname === "Password") {
+        input_type = "password"
+    }
+
+    if (editing) {
+        placeholder = "Type new "+fieldname+".."
+    } else {
+        placeholder = "fieldvalue"
+    }
+
     return (
         <div className="profilefield">
             <div>{fieldname}</div>
-            <input type="text" className="field-input" placeholder={"Type new "+fieldname+".."}></input>
+            <input type={input_type} className="field-input" placeholder={placeholder}></input>
         </div>
     )
 }
 
 const Profile: React.FC = () => {
+    
+    function Edit() {
+        editing = true;
+    }
+
     return (
         <div className="container">
+            <button className="submit-button" onClick={Edit}>Edit</button>
             <Field fieldname="Email"></Field>
             <Field fieldname="Password"></Field>
             <Field fieldname="First Name"></Field>
