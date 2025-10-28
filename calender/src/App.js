e08e0542d71a73b753dc5bdfc6ebf256db6bd89a
Kai <kaitholen@outlook.com>
2025-10-28 13:41:47 +0100

    merge fix 2

--- calender/src/App.js
+++ calender/src/App.js
@@ -1,14 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
-import Profile from './screens/Profile/Profile.tsx';
+import './styling/style.css';
+import Login from './screens/Login/Login.tsx';
+import CalendarScreen from './screens/calendar/calendar-screen.tsx';
+import AdminDashboard from './screens/admin/dashboard.tsx';
+import Navbar from './components/navbar/navbar.tsx';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import RoomMap from './screens/rooms/roomMap.tsx';
+import Room from './screens/rooms/room.tsx';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Profile></Profile>
-      </header>
-      
+      <BrowserRouter>
+        <Navbar></Navbar>
+
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path='/calendar' element={<CalendarScreen />} />
+          <Route path="/" element={<Login />} />
+          <Route path="/admin" element={<AdminDashboard />} />
+          <Route path="/rooms" element={<RoomMap />} />
+          <Route path="/rooms/:id" element={<Room />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
