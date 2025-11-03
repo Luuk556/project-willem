1d89360d556bfe96992f67fad5940b415ccbb7a4
Kai <kaitholen@outlook.com>
2025-11-03 18:48:33 +0100

    changed profile screen still needs css

--- calender/src/screens/Profile/Profile.tsx
+++ calender/src/screens/Profile/Profile.tsx
@@ -1,30 +1,49 @@
-import React, { useState } from "react"
+import React, { ProfilerProps, useState } from "react"
 import "./Profile.css"
 
-const Profile: React.FC = () => {
-    const mockdata = ["username", "email", "password"]
+const mockdata = ["username", "email", "password"]
 
-    const [username, setUsername] = useState(mockdata[0])
-    const [email, setEmail] = useState(mockdata[1])
-    const [password, setPassword] = useState(mockdata[2])
+interface EditprofileProps {
+    oldusername: string,
+    oldemail: string,
+    oldpassword: string,
+    oldimg: string,
+    oldbio: string,
+}
 
-    const handleSubmit = (e: any) => {
-        e.preventDefault()
+interface profileProps {
+    username: string,
+    email: string,
+    password: string,
+    img: string,
+    bio: string,
+}
 
-        const profile = {username, email, password}
+const handleSubmit = (e: any, profileData: {}) => {
+    e.preventDefault()
 
-        fetch('http://localhost:8000/profile', {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(profile)
-        }).then(() => {
-            console.log("Profile data updated")
-        })
-    }
+    fetch('http://localhost:8000/profile', {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(profileData)
+    }).then(() => {
+        console.log("Profile data updated")
+    })
+}
 
+
+function ProfileEdit({oldusername, oldemail, oldpassword, oldimg, oldbio}: EditprofileProps) {
+
+    const [username, setUsername] = useState(oldusername)
+    const [email, setEmail] = useState(oldemail)
+    const [password, setPassword] = useState(oldpassword)
+    const [img, setPicture] = useState(oldimg)
+    const [bio, setBio] = useState(oldbio)
+
+    const profileData = {username, email, password, img}
     return (
-        <div className="container">
-            <form onSubmit={handleSubmit}>
+        <div className="profile">
+            <form onSubmit={(event) => handleSubmit(event, profileData)}>
                 <label>Username</label>
                 <input
                 type="text"
@@ -55,4 +74,35 @@ const Profile: React.FC = () => {
     )
 }
 
-export default Profile;
+function Profile({username, email, password, img, bio}: profileProps) {
+    const [editing, setEditing] = useState(false)
+
+    const editProfileView = <div>
+                            <ProfileEdit oldusername={username} oldemail={email} oldpassword={password} oldimg={img} oldbio={bio}></ProfileEdit>
+                            </div>
+    var profileView = <div className="profile">
+        <h1 className="profile-field">Username: {username}</h1>
+        <h1 className="profile-field">Email: {email}</h1>
+        <h1 className="profile-field">Password: {password}</h1>
+        <h1 className="profile-field">Bio: {bio}</h1>
+        <button onClick={(event) => setEditing(true)}>Edit</button>
+    </div>
+
+    if (editing) {
+        profileView = editProfileView
+    }
+
+    return (
+        <div>
+            {profileView}
+        </div>
+    )
+}
+
+export default function() {
+    return (
+        <div>
+            <Profile username="kai" email="kai@gmail.com" password="secret" img="" bio="hellooorrr"></Profile>
+        </div>
+    )
+};
