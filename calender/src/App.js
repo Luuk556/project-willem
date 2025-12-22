087802cfd1940406bbc74ae53d307a96c40ca6b6
luuk556 <luukweeda.2001@gmail.com>
2025-12-22 15:29:28 +0100

    Resolved merge conflict.

--- calender/src/App.js
+++ calender/src/App.js
@@ -1,12 +1,7 @@
 import './App.css';
 import './styling/style.scss';
-import Login from './screens/Login/Login.tsx';
-import CalendarScreen from './screens/calendar/calendar-screen.tsx';
-import AdminRoomDashboard from './screens/admin/dashboard/room.tsx';
-import AdminEventDashboard from './screens/admin/dashboard/event.tsx';
 import Navbar from './components/navbar/navbar.tsx';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Navbar from './components/navbar/navbar.tsx';
 import Home from './components/Home/Home.tsx';
 import Login from './components/profile/Login.tsx';
 import CalendarScreen from './components/calendar/calendar-screen.tsx'
@@ -28,7 +23,6 @@ function App() {
           <Route path='/calendar' element={<CalendarScreen />} />
           <Route path="/" element={<Login />} />
           <Route path="/Admin/room-dashboard" element={<AdminRoomDashboard />} />
-          <Route path="/Admin/event-dashboard" element={<AdminEventDashboard />} />
           <Route path="/rooms" element={<RoomMap />} />
           <Route path="/rooms/:id" element={<Room />} />
           <Route path="/profile" element={<Profile />} />
